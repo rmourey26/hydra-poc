0734d1050fcb30b040c17a0f7ab2c35ecac7c30a
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-03-15 18:22:29 +0100

    Try to extend flake.nix devShell with cardano node exes
    'nix develop' segfaults though

--- flake.nix
+++ flake.nix
@@ -10,16 +10,26 @@
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    cardanoNodeSrc = {
+      url = "github:input-output-hk/cardano-node";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, cardanoNodeSrc }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         overlays = [
           haskellNix.overlay
+          # Cardano-node packages overlay (using haskell.nix)
+          # HACK(SN): gitrev does not compose
+          (pkgs: _: with pkgs; {
+            gitrev = "0b0ab070e71f7f29bb35dfc595c6a69772b1d866";
+          })
+          (import "${cardanoNodeSrc}/nix/pkgs.nix")
+          # This overlay adds our project to pkgs
+          # REVIEW(SN): why an overlay?
           (final: prev: {
-            # This overlay adds our project to pkgs
-            # REVIEW(SN): why an overlay?
             hydraProject =
               final.haskell-nix.project' {
                 src = ./.;
@@ -32,7 +42,7 @@
       in
       flake // {
         # Built by `nix build`
-        defaultPackage = flake.packages."local-cluster:exe:local-cluster";
+        defaultPackage = flake.packages." local-cluster:exe:local-cluster ";
 
         # Shell provided to `nix develop`
         devShell = pkgs.hydraProject.shellFor {
@@ -40,7 +50,14 @@
             cabal = "latest";
             hlint = "latest";
             haskell-language-server = "latest";
+            fourmolu = "latest";
           };
+
+          buildInputs = [
+            # From the cardano-node project overlay
+            pkgs.cardano-node
+            pkgs.cardano-cli
+          ];
         };
       });
 }
