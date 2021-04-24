226a49dbf76abb321959018a6a650ccbcece2d66
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-04-24 13:58:20 +0000

    Introduced hydra-model package

---
+++ hydra-model/src/Hydra/Model.hs
@@ -0,0 +1,2 @@
+-- | A high-level model for a cluster of Hydra nodes
+module Hydra.Model where
---
+++ hydra-model/test/Hydra/ModelSpec.hs
@@ -0,0 +1,8 @@
+module Hydra.ModelSpec where
+
+import Cardano.Prelude
+import Test.Hspec (Spec, describe, it, pendingWith)
+
+spec :: Spec
+spec = describe "Hydra Nodes Model" $ do
+  it "checks behavior of a 2 nodes cluster" $ pendingWith "Not implemented"
